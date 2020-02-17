c915eafaa0613832894107ebfc304de0e203b05a
greenyjwk <greenyjwk@gmail.com>
2020-02-17 11:53:44 -0500

    Modify: Added background image in the second half page and implemented the smooth scroll function(In progress)

--- app.js
+++ app.js
@@ -2,25 +2,29 @@ function nextPage( target, duration){
 
     var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect().top;
-
     var startPosition = window.pageYOffset;
-
     var distance = targetPosition - startPosition;
-
     var startTime = null;
 
     console.log(startPosition);
-
     function animation(currentTime){
         if(startTime === null){
             startTime = currentTime;
         }
-        var timeElapsed = currentTime = startTime;  
+        var timeElapsed = currentTime - startTime;  
+        var run = ease( timeElapsed , startPosition , distance , duration );
+        window.scroll( 0 , run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
     }
+
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    }
+
     requestAnimationFrame(animation);     
 }
 
-
-// for test
-
-nextPage('.section1', 1000 );
+nextPage('.firstPageClick', 1000 );
