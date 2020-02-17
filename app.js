63d46a5caa9439787360a6bf6ffeb306e67cee8b
greenyjwk <greenyjwk@gmail.com>
2020-02-16 21:46:03 -0500

    Modify: Added smooth scroll function (In Progress)

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 function nextPage( target, duration){
 
     var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+
+    var startPosition = window.pageYOffset;
+
+    var distance = targetPosition - startPosition;
+
+    var startTime = null;
+
+    console.log(startPosition);
+
+    function animation(currentTime){
+        if(startTime === null){
+            startTime = currentTime;
+        }
+        var timeElapsed = currentTime = startTime;  
+    }
+    requestAnimationFrame(animation);     
 }
+
+nextPage('.section1', 1000 );
