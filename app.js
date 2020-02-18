195b2b9fb5e12ec11821f1ddf3d33f0a46073aa2
greenyjwk <greenyjwk@gmail.com>
2020-02-17 19:03:21 -0500

    Modify : Implemented smooth scroll function in JS

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ function nextPage( target, duration){
     var distance = targetPosition - startPosition;
     var startTime = null;
 
-    console.log(startPosition);
     function animation(currentTime){
         if(startTime === null){
             startTime = currentTime;
@@ -23,8 +22,11 @@ function nextPage( target, duration){
         t--;
         return -c/2 * (t*(t-2) - 1) + b;
     }
-
     requestAnimationFrame(animation);     
 }
 
-nextPage('.firstPageClick', 1000 );
+var section = document.querySelector('.firstPageClick');
+
+section.addEventListener( 'click', function(){
+    nextPage('.secondPageClick' , 3000);
+});
